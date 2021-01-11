c0de134b728cb204b80b90ffcd59c7a985af7df8
Dionison <krekmg@gmail.com>
2021-01-11 03:37:17 -0300

    add model database

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,7 +1,43 @@
 import connect from '../../utils/database';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
-    res.status(200).json({msg: 'eh nois'});
+interface ErrorResponseType {
+    error: string
 }
 
-const { db } = await connect();
+interface SuccessResponseType {
+    _id: string;
+    name: string;
+    email: string;
+    cellphone: string;
+    teacher: string;
+}
+export default async (
+    req: NextApiRequest, 
+    res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+    ): Promise<void> => {
+
+    if (req.method === 'POST') {
+        
+        const { name, email, cellphone, teacher} = req.body;
+        
+        if (!name || !email || !cellphone || !teacher) {
+            res.status(400).json({ error: 'Missing some parameter' });
+            return;
+        }
+        
+        const { db } = await connect();
+        
+        const response = await db.collection('users').insertOne ({  
+            name, 
+            email, 
+            cellphone, 
+            teacher 
+        });
+        
+        res.status(200).json(response.ops[0]);
+    } else {
+        res.status(400).json({ error: req.body });
+    }
+}
+
